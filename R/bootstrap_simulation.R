787b636df98309af5851af088fc51e520f3230ab
Stijn Masschelein <stijnmasschelein@gmail.com>
2018-12-17 11:19:20 +0800

    boostrap

---
+++ R/bootstrap_simulation.R
@@ -0,0 +1,14 @@
+source("R/parameters.R")
+library(simcompl2)
+
+data_params_new = data_params
+data_params_new$rate = data_params$rate[c(1, 3, 5)]
+test_params_new = test_params[2]
+sim_params_new = sim_params 
+sim_params_new$boot = TRUE
+sim_params_new$bootR = 500
+boot_simulation = do.call(run_simulation, 
+                          list(data_params = data_params_new,
+                               test_params = test_params_new, 
+                               sim_params = sim_params_new))
+saveRDS(boot_simulation, "simulated_data/boot_simulation.Rds")
