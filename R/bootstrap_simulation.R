1d842bf58b43c9e8866797fef0409e502e2282c9
Stijn Masschelein <stijnmasschelein@gmail.com>
2018-12-19 17:16:18 +0800

    bootstrap for AWS

--- R/bootstrap_simulation.R
+++ R/bootstrap_simulation.R
@@ -3,10 +3,11 @@ library(simcompl2)
 
 data_params_new = data_params
 data_params_new$rate = data_params$rate[c(1, 3, 5)]
+data_params_new$g1 = data_params$g1[c(2, 3)]
 test_params_new = test_params[2]
 sim_params_new = sim_params 
 sim_params_new$boot = TRUE
-sim_params_new$bootR = 500
+sim_params_new$bootR = 2000
 boot_simulation = do.call(run_simulation, 
                           list(data_params = data_params_new,
                                test_params = test_params_new, 
