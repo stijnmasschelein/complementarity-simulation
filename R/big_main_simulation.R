d5c55c61f56a50f9c6edc2fbdccb05536e69d6f8
Stijn Masschelein <stijnmasschelein@gmail.com>
2019-07-14 11:20:32 +0800

    big main ready for AWS

--- R/big_main_simulation.R
+++ R/big_main_simulation.R
@@ -1,9 +1,10 @@
 library(simcompl2)
 source("R/big_parameters.R")
-sim_params$nsim = 2; sim_params$mc_cores = 4
-
 big_main_simulation = 
   do.call(run_simulation, list(data_params = data_params,
                                test_params = test_params,
                                sim_params = sim_params))
 saveRDS(big_main_simulation, "simulated_data/big_main_simulation.Rds")
+pbPost("note", "Big Main Simulation", 
+       "It's done. You should close the AWS instance and commit 
+       and push the result from the small instance.")
