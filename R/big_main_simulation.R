4930c3bd9cc942fbf394b9bff5f5bd572c900c38
Stijn Masschelein <stijnmasschelein@gmail.com>
2019-07-14 19:32:12 +0800

    adjustment for spot instance

--- R/big_main_simulation.R
+++ R/big_main_simulation.R
@@ -1,10 +1,12 @@
 library(simcompl2)
 source("R/big_parameters.R")
+start_time = Sys.time()
 big_main_simulation = 
   do.call(run_simulation, list(data_params = data_params,
                                test_params = test_params,
                                sim_params = sim_params))
-saveRDS(big_main_simulation, "simulated_data/big_main_simulation.Rds")
+end_time = Sys.time()
+msg = end_time - start_time
 pbPost("note", "Big Main Simulation", 
-       "It's done. You should close the AWS instance and commit 
-       and push the result from the small instance.")
+       paste("It's done. You should close the AWS instance and commit and push the result from the small instance.", 
+       msg, sep = "\n"))
--- R/big_parameters.R
+++ R/big_parameters.R
@@ -1,15 +1,16 @@
-nobs    = list(100, 200, 300)
+nobs    = list(300)
 g1      = list(c(.33, 0, 0), c(.33, .33, 0), c(.33, -.33, 0))
 sd_eps  = list(c(.5, .5, 0), c(1, 1, 0), c(2, 2, 0))
+sd      = list(1, 2, 4)
 b1      = list(c(0, 0, 0, 0), c(0, 1, 1, 0), c(1, 1, 1, 0),
-               c(0, -1, 1, 0), c(1, -1, 1, 0))
+               c(0, -1, 1, 0))
 b2      = list(c(0, 0, 0), c(.25, 0, 0), c(.5, 0, 0))
 rate    = list(1/2, 1/4, 1/8, 1/16, 1/32)
 d       = list(c(0, 0, 0), c(.25, .25, .25), c(1, 1, 0),
-               c(1/3, 3, 0), c(1/2, 2, 0))
+               c(1/3, 3, 0))
 
 data_params = list(obs = nobs, rate = rate, b2 = b2, g1 = g1, 
-                   d = d, sd_eps = sd_eps)
+                   d = d, sd_eps = sd_eps, b1 = b1, sd = sd)
 perf_form1 = y ~ x1*x2 + x1*z + x2*z + I(x1^2) + I(x2^2)
 test_params = list(list(formula = x1 ~ x2 + z, 
                         variable = "x2",
@@ -19,4 +20,4 @@ test_params = list(list(formula = x1 ~ x2 + z,
                         variable = "x1:x2",
                         label = "performance~1",
                         nearly_correction = FALSE))
-sim_params = list(nsim = 1000, boot = FALSE, mc_cores = 36) 
+sim_params = list(nsim = 1000, boot = FALSE, mc_cores = 32) 
