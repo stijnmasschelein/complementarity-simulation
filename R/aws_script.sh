1d723a45cd0d37ccd3f02fc469742bc352a4aeb3
Stijn Masschelein <stijnmasschelein@gmail.com>
2019-07-15 14:57:42 +0800

    update aws script

--- R/aws_script.sh
+++ R/aws_script.sh
@@ -1,12 +1,16 @@
-#! bin/bash
+#! /bin/bash
 
+sudo apt install apt-transport-https software-properties-common 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-sudo add-apt-repository 'deb [arch=amd64,i386] https://cran.rstudio.com/bin/linux/ubuntu xenial/'
-sudo apt-get install r-base
+sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
+sudo apt update
+sudo apt install r-base
 
+mkdir simulation
+cd simulation
 git init
 git remote add simulation https://github.com/stijnmasschelein/complementarity-simulation.git
-git fetch simulation
+git fetch simulation master
+git pull simulation master
 
-cd complementarity_simulation/overleaf
 Rscript R/big_main_simulation_additional.R
